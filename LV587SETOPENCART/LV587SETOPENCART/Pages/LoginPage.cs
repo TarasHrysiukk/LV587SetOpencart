6a31a8c2e2fdb369d1d69073d3373f59cd77ab73
TarasHrysiukk <t.grusyk@gmail.com>
2021-06-01 21:41:03 +0300

    add some Page Object test Code

--- LV587SETOPENCART/LV587SETOPENCART/Pages/LoginPage.cs
+++ LV587SETOPENCART/LV587SETOPENCART/Pages/LoginPage.cs
@@ -10,23 +10,37 @@ namespace LV587SETOPENCART.Pages
     class LoginPage
     {
 
-        //forgotten password
+        //forgotten password 
         //input email + Click
         //input password + Click
         //Login button + Click
 
         public IWebDriver driver;
         public IWebElement inputLogin { get; private set; }
-        public IWebElement inputPassword { get; private set; }
+        public IWebElement inputPass { get; private set; }
         public IWebElement loginButton { get; private set; }
 
         public LoginPage(IWebDriver driver)
         {
             this.driver = driver;
             inputLogin = driver.FindElement(By.Id("input-email"));
-            inputPassword = driver.FindElement(By.Id("input-password"));
+            inputPass = driver.FindElement(By.Id("input-password"));
             loginButton = driver.FindElement(By.XPath("//input[contains(@type,\"submit\")]"));
             // :)
+        }
+
+        //input Email
+        public void InputEmail()
+        {
+            inputLogin.Clear();
+            inputLogin.SendKeys("user1@gmal.com");
+        }
+
+        //input Password
+        public void InputPassword()
+        {
+            inputPass.Clear();
+            inputPass.SendKeys("qwerty");
         }
     }
 }
