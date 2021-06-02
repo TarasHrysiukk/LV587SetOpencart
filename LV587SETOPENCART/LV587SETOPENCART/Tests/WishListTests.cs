75be55613158d183ee25baa99fe4987abfd72af5
Slupachyk <slupachykdasha@gmail.com>
2021-06-02 23:46:42 +0300

    WishList Test base added

---
+++ LV587SETOPENCART/LV587SETOPENCART/Tests/WishListTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using System.Threading;
+using LV587SETOPENCART.Pages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace LV587SETOPENCART.Tests
+{
+    class WishListTests
+    {
+        private IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void BeforeAllMethods()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        [OneTimeTearDown]
+        public void AfterAllMethods()
+        {
+            driver.Quit();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver.Navigate().GoToUrl(@"http://localhost/");
+        }
+
+        [Test]
+        public void AddToWishListTest_LoggIn()
+        {
+            LoginPage loginPage = new LoginPage(driver);
+            loginPage.InputEmailText("testuser@gmail.com");
+            Thread.Sleep(500);
+            loginPage.InputPasswordText("qwerty");
+
+            Thread.Sleep(500);
+
+            HeaderComponent header = new HeaderComponent(driver);
+            header.ChooseCategory(CategoryMenu.PhonesAndPDAs);
+
+            PageWithProducts pageWithProd = new PageWithProducts(driver);
+            pageWithProd.SelectProduct(pageWithProd.FirstProduct);
+            Thread.Sleep(500);
+
+        }
+
+    }
+}
