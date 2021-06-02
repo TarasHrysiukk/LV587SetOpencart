36bf767e6cb9c081f085bfe28fa5c8c68b0392de
Slupachyk <slupachykdasha@gmail.com>
2021-06-02 14:32:03 +0300

    Register description added

--- LV587SETOPENCART/LV587SETOPENCART/Pages/WishListPage.cs
+++ LV587SETOPENCART/LV587SETOPENCART/Pages/WishListPage.cs
@@ -6,15 +6,20 @@ using System.Threading.Tasks;
 
 namespace LV587SETOPENCART.Pages
 {
-    class WishListPage
+    class WishListPage: ClassWithDriver
     {
         //remember product name (put insude string variable)!!!!!!!!!!!!!!!!!!!!!!!!!
         //check if exist
-        
+
         //delethed first element (button)
         //add to cart element (button)
         //method ( get unit price first child )
         //button Continue (return My Account)
+
+        public WishListPage(IWebDriver driver): base(driver){}
+        
+
+
 
 
 
---
+++ LV587SETOPENCART/LV587SETOPENCART/Tests/WishListTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+
+namespace LV587SETOPENCART
+{
+    public class WishListTests
+    {
+        // private IWebDriver driver;
+        [OneTimeSetUp]
+        public void BeforeAllMethods()
+        {
+           //  driver = new ChromeDriver();
+           //  driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+        [SetUp]
+        public void Setup()
+        {
+           // driver.Navigate().GoToUrl(@"http://localhost/index.php?route=account/register");
+
+        }
+
+        [OneTimeTearDown]
+        public void AfterAllMethods()
+        {
+           // driver.Quit();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
+
