3c204f96ae6ff89c99b96ed26797ab97eabb4c6d
Маріан-Северин Шевчук <Severun2001@gmail.com>
2021-06-02 17:23:28 +0300

    Added enum menu

--- LV587SETOPENCART/LV587SETOPENCART/Pages/ProductComponents.cs
+++ LV587SETOPENCART/LV587SETOPENCART/Pages/ProductComponents.cs
@@ -7,7 +7,7 @@ using OpenQA.Selenium;
 
 namespace LV587SETOPENCART.Pages
 {
-    class ProductComponents:ClassWithDriver
+    class ProductComponents : ClassWithDriver
     {
 
         public IWebElement productPrice;
@@ -21,19 +21,19 @@ namespace LV587SETOPENCART.Pages
         public IWebElement thirdRate;
         public IWebElement fourthRate;
         public IWebElement emptyReview;
-        public ProductComponents(IWebDriver driver):base(driver)
+        public ProductComponents(IWebDriver driver) : base(driver)
         {
-            productPrice= driver.FindElement(By.CssSelector("#content > div:nth-child(1) > div.col-sm-4 > ul:nth-child(4) > li:nth-child(1) > h2"));
-            reviewButton= driver.FindElement(By.CssSelector("#content > div:nth-child(1) > div.col-sm-4 > div.rating > p > a:nth-child(6)"));
-            userNameTextBox= driver.FindElement(By.CssSelector("#input-name"));
-            userReviewTextBox= driver.FindElement(By.CssSelector("#input-review"));
+            productPrice = driver.FindElement(By.CssSelector("#content > div:nth-child(1) > div.col-sm-4 > ul:nth-child(4) > li:nth-child(1) > h2"));
+            reviewButton = driver.FindElement(By.CssSelector("#content > div:nth-child(1) > div.col-sm-4 > div.rating > p > a:nth-child(6)"));
+            userNameTextBox = driver.FindElement(By.CssSelector("#input-name"));
+            userReviewTextBox = driver.FindElement(By.CssSelector("#input-review"));
             sendReview = driver.FindElement(By.CssSelector("#button-review"));
-            zeroRate= driver.FindElement(By.CssSelector("#form-review > div:nth-child(5) > div > input[type=radio]:nth-child(2)"));
-            firstRate= driver.FindElement(By.CssSelector("#form-review > div:nth-child(5) > div > input[type=radio]:nth-child(3)"));
-            secondRate= driver.FindElement(By.CssSelector("#form-review > div:nth-child(5) > div > input[type=radio]:nth-child(4)"));
-            thirdRate= driver.FindElement(By.CssSelector("#form-review > div:nth-child(5) > div > input[type=radio]:nth-child(5)"));
-            fourthRate= driver.FindElement(By.CssSelector("#form-review > div:nth-child(5) > div > input[type=radio]:nth-child(6)"));
-            emptyReview= driver.FindElement(By.CssSelector("#review > p"));
+            zeroRate = driver.FindElement(By.CssSelector("#form-review > div:nth-child(5) > div > input[type=radio]:nth-child(2)"));
+            firstRate = driver.FindElement(By.CssSelector("#form-review > div:nth-child(5) > div > input[type=radio]:nth-child(3)"));
+            secondRate = driver.FindElement(By.CssSelector("#form-review > div:nth-child(5) > div > input[type=radio]:nth-child(4)"));
+            thirdRate = driver.FindElement(By.CssSelector("#form-review > div:nth-child(5) > div > input[type=radio]:nth-child(5)"));
+            fourthRate = driver.FindElement(By.CssSelector("#form-review > div:nth-child(5) > div > input[type=radio]:nth-child(6)"));
+            emptyReview = driver.FindElement(By.CssSelector("#review > p"));
         }
 
 
@@ -43,6 +43,15 @@ namespace LV587SETOPENCART.Pages
             return int.Parse(productPrice.Text);
         }
 
+        public enum RateChoose
+        {
+            badRate,
+            poorRate,
+            fairRate,
+            goodRate,
+            excellentRate
+        }
+
         //method view review
         public void ViewReview()
         {
@@ -50,7 +59,7 @@ namespace LV587SETOPENCART.Pages
         }
 
         //method write review (input name and etc..)
-        public void WriteReview(string userName, string userReview, int rate)
+        public void WriteReview(string userName, string userReview, RateChoose rateChoose)
         {
             userNameTextBox.Click();
             userNameTextBox.SendKeys(userName);
@@ -58,29 +67,23 @@ namespace LV587SETOPENCART.Pages
             userReviewTextBox.Click();
             userReviewTextBox.SendKeys(userReview);
 
-            if (rate==0)
+            switch (rateChoose)
             {
-                zeroRate.Click();
-            }
-
-            if(rate==1)
-            {
-                firstRate.Click();
-            }
-
-            if(rate==2)
-            {
-                secondRate.Click();
-            }
-
-            if(rate==3)
-            {
-                thirdRate.Click();
-            }
-
-            if(rate==4)
-            {
-                fourthRate.Click();
+                case RateChoose.badRate:
+                    zeroRate.Click();
+                    break;
+                case RateChoose.poorRate:
+                    firstRate.Click();
+                    break;
+                case RateChoose.fairRate:
+                    secondRate.Click();
+                    break;
+                case RateChoose.goodRate:
+                    thirdRate.Click();
+                    break;
+                case RateChoose.excellentRate:
+                    fourthRate.Click();
+                    break;
             }
 
             sendReview.Click();
@@ -91,7 +94,7 @@ namespace LV587SETOPENCART.Pages
         // if the review is present 
         public bool reviewPresent()
         {
-            if(emptyReview.Text!= "There are no reviews for this product.")
+            if (emptyReview.Text != "There are no reviews for this product.")
             {
                 return true;
             }
