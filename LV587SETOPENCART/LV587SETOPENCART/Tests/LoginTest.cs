298a25bc9a56bd29a1eef47b20d901d27f37a606
Slupachyk <slupachykdasha@gmail.com>
2021-06-03 17:11:01 +0300

    Some changes

---
+++ LV587SETOPENCART/LV587SETOPENCART/Tests/LoginTest.cs
@@ -0,0 +1,30 @@
+﻿using LV587SETOPENCART.Pages;
+using LV587SETOPENCART.BL;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+
+namespace LV587SETOPENCART.Tests
+{
+    [TestFixture]
+    class LoginTest
+    {
+        IWebDriver driver = new ChromeDriver();
+        [Test]
+        public void Test()
+        {
+            LoginBL loginBL = new LoginBL(driver);
+            MyAccountPage myAccountPage = new MyAccountPage(driver);
+            loginBL.Login("user1@gmail.com", "qwerty");
+
+            Assert.AreEqual("My Account", myAccountPage.MyAccountText());
+
+        }
+    }
+}
