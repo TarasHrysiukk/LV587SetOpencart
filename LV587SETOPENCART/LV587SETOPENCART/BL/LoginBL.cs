0432956deb3b286e0dbcef2242fb9dd91fb37891
TarasHrysiukk <t.grusyk@gmail.com>
2021-06-03 16:15:48 +0300

    LoginTest

---
+++ LV587SETOPENCART/LV587SETOPENCART/BL/LoginBL.cs
@@ -0,0 +1,26 @@
+﻿using LV587SETOPENCART.Pages;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV587SETOPENCART.BL
+{
+    class LoginBL : ClassWithDriver
+    {
+        LoginPage loginPage;
+        public LoginBL(IWebDriver driver) : base(driver)
+        {
+            loginPage = new LoginPage(driver);
+        }
+
+        public void Login(string email, string pass)
+        {
+            loginPage.InputEmailText(email);
+            loginPage.InputPasswordText(pass);
+            loginPage.ClickOnLoginButton();
+        }
+    }
+}
---
+++ LV587SETOPENCART/LV587SETOPENCART/Tests/LoginTest.cs
@@ -0,0 +1,31 @@
+﻿using LV587SETOPENCART.Pages;
+using LV587SETOPENCART.BL;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+
+namespace LV587SETOPENCART.Tests
+{
+    [TestFixture]
+    class LoginTest
+    {
+        IWebDriver driver = new ChromeDriver();
+        //LoginPage loginPage = new LoginPage(driver);
+        [Test]
+        public void Test()
+        {
+            LoginBL loginBL = new LoginBL(driver);
+            MyAccountPage myAccountPage = new MyAccountPage(driver);
+            loginBL.Login("user1@gmail.com", "qwerty");
+
+            Assert.AreEqual("My Account", myAccountPage.MyAccountText());
+
+        }
+    }
+}
