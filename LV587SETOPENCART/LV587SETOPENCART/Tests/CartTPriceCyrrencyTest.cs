b22688f617b756efb508127af19d9ad46749fd31
earthnotmars <super.3359@gmail.com>
2021-06-04 19:17:51 +0300

    Another selector of TotalPrice on CartPage (when more than 1 product in cart) Added new Test

---
+++ LV587SETOPENCART/LV587SETOPENCART/Tests/CartTPriceCyrrencyTest.cs
@@ -0,0 +1,87 @@
+﻿using LV587SETOPENCART.Pages;
+using LV587SETOPENCART.BL;
+using OpenQA.Selenium;
+using System;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using LV587SETOPENCART.Tools;
+using System.Threading;
+
+namespace LV587SETOPENCART.Tests
+{
+    [TestFixture]
+    class CartTPriceCyrrency
+    {
+        IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void BeforeAllMethods()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Window.Maximize();
+        }
+
+        [OneTimeTearDown]
+        public void AfterAllMethods()
+        {
+            driver.Quit();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver.Navigate().GoToUrl(@"https://demo.opencart.com/");
+
+        }
+
+        [Test]
+        public void DropdownCurrenciesTest()
+        {
+            string currencySymbol;
+            HeaderComponent header = new HeaderComponent(driver);
+            PageWithProducts productPage = new PageWithProducts(driver);
+            ProjectTools regex = new ProjectTools(driver);
+            ProductComponents product = new ProductComponents(driver);
+            CartPage cart = new CartPage(driver);
+            WishListPage wishList = new WishListPage(driver);
+
+            //Click on My Account > Login
+            header.ClickOnMyAccount(MyAccountMenuActions.Login);
+            //login
+            LoginBL loginBL = new LoginBL(driver);
+            loginBL.Login("iva@new.com", "qwerty");
+            //Select category "Phones & PDAs"
+            header.ChooseCategory(CategoryMenu.PhonesAndPDAs);
+            //Add first product to cart from the product list
+            productPage.ClickCartButton();
+            // Open cart page
+            header.ClickOnShoppingCartLink();
+
+            // Select 'Euro' in dropdown 'Currency'.
+            header.SelectSearch();
+            header.CurrencyClickAndSelect(Currencies.EUR);
+            currencySymbol = "€";
+            Thread.Sleep(2000);
+            //Verify that Total Price price is displayed in euro
+            bool trueCurrency = regex.PriceCurrency(cart.GetTotalPrice(), currencySymbol);
+            Assert.True(trueCurrency);
+
+            // Select 'Pound Sterling' in dropdown 'Currency'.
+            header.SelectSearch();
+            header.CurrencyClickAndSelect(Currencies.GBP);
+            currencySymbol = "£";
+            //Verify that Total price is displayed in PoundsSterling
+            trueCurrency = regex.PriceCurrency(cart.GetTotalPrice(), currencySymbol);
+            Assert.True(trueCurrency);
+
+            // Select 'US Dollars' in dropdown 'Currency'.
+            header.SelectSearch();
+            header.CurrencyClickAndSelect(Currencies.USD);
+            currencySymbol = "$";
+            //Verify that Total price is displayed in USA Dollars 
+            trueCurrency = regex.PriceCurrency(cart.GetTotalPrice(), currencySymbol);
+            Assert.True(trueCurrency);
+        }
+    }
+}
