f9a78f9a14ff33eb42f7196b5ca46fb00024ef01
Slupachyk <slupachykdasha@gmail.com>
2021-06-04 13:05:34 +0300

    Some selectors in WishList were changed WishListTest`s some bug were fixed

--- LV587SETOPENCART/LV587SETOPENCART/Pages/WishListPage.cs
+++ LV587SETOPENCART/LV587SETOPENCART/Pages/WishListPage.cs
@@ -19,9 +19,9 @@ namespace LV587SETOPENCART.Pages
         public IWebElement PageTitle { get { return driver.FindElement(By.CssSelector("#content h2")); } } //  PageTitle ("Wish List")
         public IWebElement ProductName { get { return driver.FindElement(By.CssSelector(".table-hover tr:first-child  td.text-left a")); } } // Name of product in first row
         public IWebElement UnitPrice { get { return driver.FindElement(By.CssSelector("tr:first-child .price")); } } // Unit Price of product in first row
-        public IWebElement AddToCartButton { get { return driver.FindElement(By.CssSelector("tr:first-child .fa-shopping-cart")); } } // Select AddToCart Button in a first row
-        public IWebElement RemoveButton { get { return driver.FindElement(By.CssSelector("tr:first-child .fa-times")); } } // Select Remove Button in a first row
-        public IWebElement ContinueButton { get { return driver.FindElement(By.CssSelector(".pull-right .btn")); } } // Select Continue Button 
+        public IWebElement AddToCartButton { get { return driver.FindElement(By.CssSelector("#content tr:first-child button")); } } // Select AddToCart Button in a first row
+        public IWebElement RemoveButton { get { return driver.FindElement(By.CssSelector("#content tr:first-child .btn-danger")); } } // Select Remove Button in a first row
+        public IWebElement ContinueButton { get { return driver.FindElement(By.CssSelector("#content .pull-right .btn-primary")); } } // Select Continue Button 
 
         public IWebElement AlertMessage { get { return driver.FindElement(By.CssSelector(".alert-success:not( .fa-check-circle)")); } }
         public WishListPage(IWebDriver driver) : base(driver) { }
