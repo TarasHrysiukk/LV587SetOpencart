9e5e1657cb87cda1a5caa109d9b3a5818183c5a5
TarasHrysiukk <t.grusyk@gmail.com>
2021-06-04 11:22:17 +0300

    redesigne loginForm

--- LV587SETOPENCART/LV587SETOPENCART/Pages/ForgottenPasswordPage.cs
+++ LV587SETOPENCART/LV587SETOPENCART/Pages/ForgottenPasswordPage.cs
@@ -10,15 +10,15 @@ namespace LV587SETOPENCART.Pages
     class ForgottenPasswordPage : ClassWithDriver
     {
        // LoginPage loginPage;
-        public IWebElement TitleForgotPassword { get; private set; }
-        public IWebElement ContinueButton { get; private set; }
-        public IWebElement InputEmail { get; private set; }
+        //public IWebElement TitleForgotPassword { get; private set; }
+        public IWebElement ContinueButton { get {return driver.FindElement(By.CssSelector("input[type='submit']")); } }
+        public IWebElement InputEmail { get {return driver.FindElement(By.CssSelector(".col-sm-10 > input")); } }
 
         public ForgottenPasswordPage(IWebDriver driver) : base(driver)
         {
-            InputEmail = driver.FindElement(By.CssSelector(".col-sm-10 > input"));
+            //InputEmail = driver.FindElement(By.CssSelector(".col-sm-10 > input"));
             //TitleForgotPassword = driver.FindElement(By.CssSelector("#content h1"));
-            ContinueButton = driver.FindElement(By.CssSelector("input[type='submit']"));
+            //ContinueButton = driver.FindElement(By.CssSelector("input[type='submit']"));
             //loginPage = new LoginPage(driver);
         }
 
