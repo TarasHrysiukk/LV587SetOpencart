61d955d47888efb3c0cae65fac0a207aa1da43ea
TarasHrysiukk <t.grusyk@gmail.com>
2021-06-09 21:39:38 +0300

    Login

--- LV587SETOPENCART/LV587SETOPENCART/Tests/LoginFormTest/ForgottenPasswordTest.cs
+++ LV587SETOPENCART/LV587SETOPENCART/Tests/LoginFormTest/ForgottenPasswordTest.cs
@@ -48,24 +48,26 @@ namespace LV587SETOPENCART.Tests
         [Description("This test checks to if user can input their email to if he forgot password")]
         public void ForgottenPasswordPageTest()
         {
-            //Click on My Account > Login
-            HeaderComponent headerComponent = new HeaderComponent(driver);
-            headerComponent.ClickOnMyAccount(MyAccountMenuActions.Login);
-            Thread.Sleep(2000);  //Only for presentation (works Without it)
-
-            // Click on "Forgotten Password" link text and input email
-            ForgottenPasswordBL forgottenPasswordBL = new ForgottenPasswordBL(driver);
-            forgottenPasswordBL.ForgottenPassword("user1@gmail.com");
-            Thread.Sleep(2000);  //Only for presentation (works Without it)
-
-            //Assert
-            ForgottenPasswordPage forgottenPasswordPage = new ForgottenPasswordPage(driver);
-            string expRes = "An email with a confirmation link has been sent your email address.";
-            var actRes = forgottenPasswordPage.AlertMessageText();
-            //Assert.AreEqual(expRes, actRes);
             Screenshot AfterTestScreen = ((ITakesScreenshot)driver).GetScreenshot();
             try
             {
+                //Click on My Account > Login
+                HeaderComponent headerComponent = new HeaderComponent(driver);
+                headerComponent.ClickOnMyAccount(MyAccountMenuActions.Login);
+                Thread.Sleep(2000);  //Only for presentation (works Without it)
+
+                // Click on "Forgotten Password" link text and input email
+                ForgottenPasswordBL forgottenPasswordBL = new ForgottenPasswordBL(driver);
+                forgottenPasswordBL.ForgottenPassword("user1@gmail.com");
+                Thread.Sleep(2000);  //Only for presentation (works Without it)
+
+                //Assert
+                ForgottenPasswordPage forgottenPasswordPage = new ForgottenPasswordPage(driver);
+                string expRes = "An email with a confirmation link has been sent your email address.";
+                var actRes = forgottenPasswordPage.AlertMessageText();
+                //Assert.AreEqual(expRes, actRes);
+                
+            
                 Assert.AreEqual(expRes, actRes);
             }
             catch (Exception) //Take a ScreenShot if test is failed
