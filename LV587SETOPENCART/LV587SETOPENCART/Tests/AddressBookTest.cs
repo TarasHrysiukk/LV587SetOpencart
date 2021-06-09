e32bf115ef9bd90fcad044be582ab96ae46abdc3
Andrew <orlykandiy@gmail.com>
2021-06-09 18:00:46 +0300

    Added tests

---
+++ LV587SETOPENCART/LV587SETOPENCART/Tests/AddressBookTest.cs
@@ -0,0 +1,115 @@
+﻿using Allure.Commons;
+using LV587SETOPENCART.BL;
+using LV587SETOPENCART.Pages;
+using NUnit.Allure.Attributes;
+using NUnit.Allure.Core;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LV587SETOPENCART.Tests
+{
+    [TestFixture]
+    [AllureNUnit]
+    [AllureSuite("[AddressBook]")]
+    [AllureDisplayIgnored]
+    class AddressBookTest
+    {
+        IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void BeforeAllMethods()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Window.Maximize();
+        }
+
+        [OneTimeTearDown]
+        public void AfterAllMethods()
+        {
+            driver.Quit();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            ClassWithDriver classWithDriver = new ClassWithDriver(driver);
+            classWithDriver.NavigateToURL();
+        }
+
+        [Test]
+        [AllureTag("OpenCart: NewAddress Book Test")]
+        [AllureSeverity(SeverityLevel.critical)]
+        [AllureOwner("Andrii Orlyk")]
+        [Description("This test checks to if address book was added")]
+        public void NewAddressBookTest()
+        {
+
+            HeaderComponent headerComponent = new HeaderComponent(driver);
+            headerComponent.ClickOnMyAccount(MyAccountMenuActions.Login);
+
+            LoginBL loginBL = new LoginBL(driver);
+            loginBL.Login("user1@gmail.com", "qwertyasdf12345678");
+
+            AddEditAddress addEditAddress = new AddEditAddress(driver);
+            AddressBook addressBook = new AddressBook(driver);
+            addressBook.ClickOnAddressButton();
+            addressBook.ClickOnAddAdressButton();
+            addEditAddress.AddNewAddress();
+            var actual = addressBook.GetAlertMessageText();
+
+            Assert.IsTrue(actual.Contains("Your address has been successfully added"));
+        }
+
+        [Test]
+        [AllureTag("OpenCart: DeleteAddress Book Test")]
+        [AllureSeverity(SeverityLevel.critical)]
+        [AllureOwner("Andrii Orlyk")]
+        [Description("This test checks to if address book was deleted")]
+        public void DeleteAddressBookTest()
+        {
+            HeaderComponent headerComponent = new HeaderComponent(driver);
+            headerComponent.ClickOnMyAccount(MyAccountMenuActions.Login);
+
+            LoginBL loginBL = new LoginBL(driver);
+            loginBL.Login("user1@gmail.com", "qwertyasdf12345678");
+            AddressBook addressBook = new AddressBook(driver);
+            addressBook.ClickOnAddressButton();
+            addressBook.ClickOnDeleteAdressButton();
+            var actual = addressBook.GetAlertMessageText();
+
+            Assert.IsTrue(actual.Contains("Your address has been successfully deleted"));
+        }
+
+        [Test]
+        [AllureTag("OpenCart: EditAddress Book Test")]
+        [AllureSeverity(SeverityLevel.critical)]
+        [AllureOwner("Andrii Orlyk")]
+        [Description("This test checks to if address book was deleted")]
+        public void EditAddressBookTest()
+        {
+            HeaderComponent headerComponent = new HeaderComponent(driver);
+            headerComponent.ClickOnMyAccount(MyAccountMenuActions.Login);
+
+            LoginBL loginBL = new LoginBL(driver);
+            loginBL.Login("user1@gmail.com", "qwertyasdf12345678");
+            AddressBook addressBook = new AddressBook(driver);
+            addressBook.ClickOnAddressButton();
+            addressBook.ClickOnEditAdressButton();
+            AddEditAddress addEditAddress = new AddEditAddress(driver);
+            addEditAddress.SetCity("Kiev");
+            addEditAddress.Continue();
+
+            var actual = addressBook.GetAlertMessageText();
+
+            Assert.IsTrue(actual.Contains("Your address has been successfully updated"));
+        }
+    }
+}
